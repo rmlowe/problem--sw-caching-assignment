755f3f1820d7a4adad9e708fa5634acfe10402fd
Robert Lowe <robertmlowe@rmlowe.com>
2020-10-08 23:23:06 +0800

    Assignment 2: Time to Practice: Service Workers & Caching

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,8 +1,18 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
 
 var box = document.querySelector('.box');
 var button = document.querySelector('button');
 
-button.addEventListener('click', function(event) {
+button.addEventListener('click', function (event) {
   if (box.classList.contains('visible')) {
     box.classList.remove('visible');
   } else {
